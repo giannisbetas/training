7084e3ee9d22c06dcb65d6a7f361151308da81e9
HashiCorp Training <training@hashicorp.com>
2017-06-13 15:08:40 +0000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,46 @@
+#
+# DO NOT DELETE THESE LINES!
+#
+# Your AMI ID is:
+#
+#     ami-a4f9f2c2
+#
+# Your subnet ID is:
+#
+#     subnet-9a479ed3
+#
+# Your security group ID is:
+#
+#     sg-00100979
+#
+# Your Identity is:
+#
+#     hdays-michel-haddock
+#
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+terraform {
+  backend "atlas" {
+    name = "giannis/training"
+  }
+}
+
+resource "aws_instance" "web" {
+  count                  = "${var.num_webs}"
+  ami                    = "ami-a4f9f2c2"
+  instance_type          = "t2.micro"
+  subnet_id              = "subnet-9a479ed3"
+  vpc_security_group_ids = ["sg-00100979"]
+
+  tags {
+    Identity = "hdays-michel-haddock"
+    Tag01    = "test01"
+    Tag02    = "test02"
+    Name     = "web ${count.index +1}/${var.num_webs}"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+#####  DNSIMPLE  #####
+
+variable "dnsimple_account" {}
+variable "dnsimple_token" {}
+
+variable "dnsimple_domain" {}
+
+
+#####  AWS  #####
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable "aws_region" {
+  default = "eu-west-1"
+}
+
+variable "num_webs" {
+  default = 2
+}
